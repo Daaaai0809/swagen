a64ebfdc745275b3444e2c808d66e9f311777225
Daaaai0809 <dai.tsuruga@80and.co>
2024-07-15 22:40:50 +0900

    feat #2 : input methods are abstracted with RootInputs

--- input/paths/root.go
+++ input/paths/root.go
@@ -6,159 +6,142 @@ import (
 
 	"github.com/Daaaai0809/swagen/constant"
 	"github.com/Daaaai0809/swagen/generate/methods"
-	"github.com/Daaaai0809/swagen/iuput"
+	input "github.com/Daaaai0809/swagen/input"
 	"github.com/spf13/cobra"
 )
 
-type GetPathInputs struct {
-	cmd         *cobra.Command
-	fileName    string
-	operationID string
-	summary     string
-	description string
-	tags        []string
-	security    []string
-	parameters  methods.Parameters
-	responses   methods.Responses
+type IRootInputs interface {
+	ReadAll()
+	SetFileName(string)
+	GetFileName() string
+	ReadFileName()
+	SetOperationID(string)
+	GetOperationID() string
+	ReadOperationID()
+	SetSummary(string)
+	GetSummary() string
+	ReadSummary()
+	SetDescription(string)
+	GetDescription() string
+	ReadDescription()
+	SetTags([]string)
+	GetTags() []string
+	ReadTags()
+	SetSecurity([]string)
+	GetSecurity() []string
+	ReadSecurity()
+	SetParameters(methods.Parameters)
+	GetParameters() methods.Parameters
+	ReadParameters()
+	SetResponses(methods.Responses)
+	GetResponses() methods.Responses
 }
 
-func NewGetPathInputs(cmd *cobra.Command) *GetPathInputs {
-	return &GetPathInputs{
-		cmd:         cmd,
-		fileName:    "",
-		operationID: "",
-		summary:     "",
-		description: "",
-		tags:        []string{},
-		security:    []string{},
-		parameters:  methods.Parameters{},
-		responses:   methods.Responses{},
-	}
+type RootInputs struct {
+	Cmd         *cobra.Command
+	FileName    string
+	OperationID string
+	Summary     string
+	Description string
+	Tags        []string
+	Security    []string
+	Parameters  methods.Parameters
+	Responses   methods.Responses
 }
 
-// The ReadAll method reads all the input required to define an endpoint.
-func (p *GetPathInputs) ReadAll() {
-	p.ReadFileName()
-	p.ReadOperationID()
-
-	if ok := input.YesNoPrompt(p.cmd, "Do you want to add a summary?"); ok {
-		p.ReadSummary()
-	}
-
-	if ok := input.YesNoPrompt(p.cmd, "Do you want to add a description?"); ok {
-		p.ReadDescription()
-	}
-
-	if ok := input.YesNoPrompt(p.cmd, "Do you want to add tags?"); ok {
-		p.ReadTags()
-	}
-
-	if ok := input.YesNoPrompt(p.cmd, "Do you want to add security?"); ok {
-		p.ReadSecurity()
-	}
-
-	if ok := input.YesNoPrompt(p.cmd, "Do you want to add parameters?"); ok {
-		p.ReadParameters()
-	}
-
-	if ok := input.YesNoPrompt(p.cmd, "Do you want to add responses?"); ok {
-		p.ReadResponses()
-	}
+func (p *RootInputs) SetFileName(fileName string) {
+	p.FileName = fileName
 }
 
-func (p *GetPathInputs) SetFileName(fileName string) {
-	p.fileName = fileName
-}
-
-func (p *GetPathInputs) GetFileName() string {
-	return p.fileName
+func (p *RootInputs) GetFileName() string {
+	return p.FileName
 }
 
 // The ReadFileName method takes input from the CLI to define the file name for the endpoint.
-func (p *GetPathInputs) ReadFileName() {
+func (p *RootInputs) ReadFileName() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	p.cmd.Println("Enter the file name: ")
+	p.Cmd.Println("Enter the file name: ")
 
 	scanner.Scan()
 
 	p.SetFileName(scanner.Text())
 }
 
-func (p *GetPathInputs) SetOperationID(operationID string) {
-	p.operationID = operationID
+func (p *RootInputs) SetOperationID(operationID string) {
+	p.OperationID = operationID
 }
 
-func (p *GetPathInputs) GetOperationID() string {
-	return p.operationID
+func (p *RootInputs) GetOperationID() string {
+	return p.OperationID
 }
 
 // The ReadOperationID method takes input from the CLI to define the operation ID for the endpoint.
-func (p *GetPathInputs) ReadOperationID() {
+func (p *RootInputs) ReadOperationID() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	p.cmd.Println("Enter the operation ID: ")
+	p.Cmd.Println("Enter the operation ID: ")
 
 	scanner.Scan()
 
 	p.SetOperationID(scanner.Text())
 }
 
-func (p *GetPathInputs) SetSummary(summary string) {
-	p.summary = summary
+func (p *RootInputs) SetSummary(summary string) {
+	p.Summary = summary
 }
 
-func (p *GetPathInputs) GetSummary() string {
-	return p.summary
+func (p *RootInputs) GetSummary() string {
+	return p.Summary
 }
 
 // The ReadSummary method takes input from the CLI to define a summary for the endpoint.
-func (p *GetPathInputs) ReadSummary() {
+func (p *RootInputs) ReadSummary() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	p.cmd.Println("Enter the summary: ")
+	p.Cmd.Println("Enter the summary: ")
 
 	scanner.Scan()
 
 	p.SetSummary(scanner.Text())
 }
 
-func (p *GetPathInputs) SetDescription(description string) {
-	p.description = description
+func (p *RootInputs) SetDescription(description string) {
+	p.Description = description
 }
 
-func (p *GetPathInputs) GetDescription() string {
-	return p.description
+func (p *RootInputs) GetDescription() string {
+	return p.Description
 }
 
 // The ReadDescription method takes input from the CLI to define the description of the endpoint.
-func (p *GetPathInputs) ReadDescription() {
+func (p *RootInputs) ReadDescription() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	p.cmd.Println("Enter the description: ")
+	p.Cmd.Println("Enter the description: ")
 
 	scanner.Scan()
 
 	p.SetDescription(scanner.Text())
 }
 
-func (p *GetPathInputs) SetTags(tags []string) {
-	p.tags = tags
+func (p *RootInputs) SetTags(tags []string) {
+	p.Tags = tags
 }
 
-func (p *GetPathInputs) GetTags() []string {
-	return p.tags
+func (p *RootInputs) GetTags() []string {
+	return p.Tags
 }
 
 // The ReadTags method takes input from the CLI to define tags for the endpoint.
 // It supports multiple tags.
-func (p *GetPathInputs) ReadTags() {
+func (p *RootInputs) ReadTags() {
 	var tags = make([]string, 0)
 
 	scaanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		p.cmd.Println("Enter a tag name: ")
+		p.Cmd.Println("Enter a tag name: ")
 
 		scaanner.Scan()
 
@@ -166,7 +149,7 @@ func (p *GetPathInputs) ReadTags() {
 
 		tags = append(tags, tag)
 
-		if ok := input.YesNoPrompt(p.cmd, "Do you want to add another tag?"); !ok {
+		if ok := input.YesNoPrompt(p.Cmd, "Do you want to add another tag?"); !ok {
 			break
 		}
 	}
@@ -174,29 +157,29 @@ func (p *GetPathInputs) ReadTags() {
 	p.SetTags(tags)
 }
 
-func (p *GetPathInputs) SetSecurity(security []string) {
-	p.security = security
+func (p *RootInputs) SetSecurity(security []string) {
+	p.Security = security
 }
 
-func (p *GetPathInputs) GetSecurity() []string {
-	return p.security
+func (p *RootInputs) GetSecurity() []string {
+	return p.Security
 }
 
 // The ReadSecurity method takes input from the CLI to define the security methods required for the endpoint.
 // It supports multiple security methods.
-func (p *GetPathInputs) ReadSecurity() {
+func (p *RootInputs) ReadSecurity() {
 	var securities = make([]string, 0)
 
 	for {
 		security, err := input.SingleSelect("Select a security name", constant.SecurityTypes)
 		if err != nil {
-			p.cmd.Println(err)
+			p.Cmd.Println(err)
 			continue
 		}
 
 		securities = append(securities, security)
 
-		if ok := input.YesNoPrompt(p.cmd, "Do you want to add another security?"); !ok {
+		if ok := input.YesNoPrompt(p.Cmd, "Do you want to add another security?"); !ok {
 			break
 		}
 	}
@@ -204,26 +187,26 @@ func (p *GetPathInputs) ReadSecurity() {
 	p.SetSecurity(securities)
 }
 
-func (p *GetPathInputs) SetParameters(parameters methods.Parameters) {
-	p.parameters = parameters
+func (p *RootInputs) SetParameters(parameters methods.Parameters) {
+	p.Parameters = parameters
 }
 
-func (p *GetPathInputs) GetParameters() methods.Parameters {
-	return p.parameters
+func (p *RootInputs) GetParameters() methods.Parameters {
+	return p.Parameters
 }
 
 // The ReadParameter method takes input from the CLI to define URL parameters.
 // It supports parameter definitions on the CLI as well as definitions in other files using Ref.
-func (p *GetPathInputs) ReadParameters() {
+func (p *RootInputs) ReadParameters() {
 	var parameters = make(methods.Parameters, 0)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		if ok := input.YesNoPrompt(p.cmd, "Ref Parameter?"); ok {
+		if ok := input.YesNoPrompt(p.Cmd, "Ref Parameter?"); ok {
 			var parameter methods.RefParameter
 
-			p.cmd.Println("Enter the Ref: ")
+			p.Cmd.Println("Enter the Ref: ")
 
 			scanner.Scan()
 
@@ -231,7 +214,7 @@ func (p *GetPathInputs) ReadParameters() {
 
 			parameters = append(parameters, &parameter)
 
-			if ok := input.YesNoPrompt(p.cmd, "Do you want to add another parameter?"); !ok {
+			if ok := input.YesNoPrompt(p.Cmd, "Do you want to add another parameter?"); !ok {
 				break
 			}
 
@@ -242,27 +225,27 @@ func (p *GetPathInputs) ReadParameters() {
 			Schema: make(map[string]string),
 		}
 
-		p.cmd.Println("Enter the parameter name: ")
+		p.Cmd.Println("Enter the parameter name: ")
 
 		scanner.Scan()
 		parameter.Name = scanner.Text()
 
 		in, err := input.SingleSelect("Select the parameter location", constant.ParamLocationsList)
 		if err != nil {
-			p.cmd.Println(err)
+			p.Cmd.Println(err)
 			continue
 		}
 
 		parameter.In = in
 
-		p.cmd.Println("Enter the parameter description: ")
+		p.Cmd.Println("Enter the parameter description: ")
 
 		scanner.Scan()
 		parameter.Description = scanner.Text()
 
 		paramType, err := input.SingleSelect("Select the parameter type", constant.SchemaTypeList)
 		if err != nil {
-			p.cmd.Println(err)
+			p.Cmd.Println(err)
 			continue
 		}
 
@@ -271,13 +254,13 @@ func (p *GetPathInputs) ReadParameters() {
 		switch paramType {
 		case constant.STRING_TYPE, constant.NUMBER_TYPE, constant.INTEGER_TYPE:
 			{
-				if ok := input.YesNoPrompt(p.cmd, "Do you want to add a format?"); !ok {
+				if ok := input.YesNoPrompt(p.Cmd, "Do you want to add a format?"); !ok {
 					break
 				}
 
 				format, err := input.SingleSelect("Select the parameter format", constant.TypeFormatMap[paramType])
 				if err != nil {
-					p.cmd.Println(err)
+					p.Cmd.Println(err)
 					continue
 				}
 
@@ -287,7 +270,7 @@ func (p *GetPathInputs) ReadParameters() {
 
 		parameters = append(parameters, &parameter)
 
-		if ok := input.YesNoPrompt(p.cmd, "Do you want to add another parameter?"); !ok {
+		if ok := input.YesNoPrompt(p.Cmd, "Do you want to add another parameter?"); !ok {
 			break
 		}
 	}
@@ -295,17 +278,17 @@ func (p *GetPathInputs) ReadParameters() {
 	p.SetParameters(parameters)
 }
 
-func (p *GetPathInputs) SetResponses(responses methods.Responses) {
-	p.responses = responses
+func (p *RootInputs) SetResponses(responses methods.Responses) {
+	p.Responses = responses
 }
 
-func (p *GetPathInputs) GetResponses() methods.Responses {
-	return p.responses
+func (p *RootInputs) GetResponses() methods.Responses {
+	return p.Responses
 }
 
 // The ReadResponses method takes input to define the response methods returned by the endpoint for each response code.
 // Currently, it only supports the Ref schema in Content Parameter, but we plan to support response type definitions on the CLI in the future.
-func (p *GetPathInputs) ReadResponses() {
+func (p *RootInputs) ReadResponses() {
 	var responses = make(methods.Responses)
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -315,23 +298,23 @@ func (p *GetPathInputs) ReadResponses() {
 
 		statusCode, err := input.SingleSelect("Select the status code", constant.StatusCodesList)
 		if err != nil {
-			p.cmd.Println(err)
+			p.Cmd.Println(err)
 			continue
 		}
 
-		p.cmd.Println("Enter the response description: ")
+		p.Cmd.Println("Enter the response description: ")
 
 		scanner.Scan()
 		response.Description = scanner.Text()
 
 		contentType, err := input.SingleSelect("Select the content type", constant.ContentTypeList)
 		if err != nil {
-			p.cmd.Println(err)
+			p.Cmd.Println(err)
 			continue
 		}
 
-		if ok := input.YesNoPrompt(p.cmd, "Do you need Ref ?"); ok {
-			p.cmd.Println("Enter the Ref: ")
+		if ok := input.YesNoPrompt(p.Cmd, "Do you need Ref ?"); ok {
+			p.Cmd.Println("Enter the Ref: ")
 
 			scanner.Scan()
 
@@ -355,7 +338,7 @@ func (p *GetPathInputs) ReadResponses() {
 
 		responses[statusCode] = response
 
-		if ok := input.YesNoPrompt(p.cmd, "Do you want to add another response?"); !ok {
+		if ok := input.YesNoPrompt(p.Cmd, "Do you want to add another response?"); !ok {
 			break
 		}
 	}
