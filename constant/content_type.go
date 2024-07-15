5bb71d7d3a108f28ce44c32c1a9020ac40846a2c
Kabos <130345626+Daaaai0809@users.noreply.github.com>
2024-07-16 01:47:59 +0900

    Release/v0.0.2 (#17)
    * Feature/#1 impl get endpoint generate (#14)
    
    * feat #1 : add constants
    
    * feat #1 : add config
    
    * feat #1 : implement get path yaml file generator
    
    * feat #1 : implement input operations
    
    * feat #1 : implement generate get, root, version commands + impl validation
    
    * fix #1 : remove main.go
    
    * feat #1 : add flags to input generated yaml file's dir path
    
    * refactor #1 : add comments and change name to GetPathInputs
    
    * chore #1 : add build file
    
    * Copy feature/#2 impl generate post endpoint schema (#16)
    
    * [feature] : implement get endpoint generator (#6)
    
    * feat #1 : add constants
    
    * feat #1 : add config
    
    * feat #1 : implement get path yaml file generator
    
    * feat #1 : implement input operations
    
    * feat #1 : implement generate get, root, version commands + impl validation
    
    * fix #1 : remove main.go
    
    * feat #1 : add flags to input generated yaml file's dir path
    
    * refactor #1 : add comments and change name to GetPathInputs
    
    * chore #1 : add build file
    
    * [feature] : implement generator of post endpoint schema (#13)
    
    * refactor #2 : remove types package and move type.go to /methods
    
    * refactor #2 : change Actions job name
    
    * feat #2 : input methods are abstracted with RootInputs
    
    * chore #2 : add content type text/csv
    
    * feat #2 : impl post definition file generator
    
    * feat #2 : add post command
    
    * chore #2 : update build file

---
+++ constant/content_type.go
@@ -0,0 +1,107 @@
+package constant
+
+const (
+	TEXT_PLAIN_ID = iota
+	TEXT_HTML_ID
+	TEXT_XML_ID
+	TEXT_CSS_ID
+	TEXT_JAVASCRIPT_ID
+	TEXT_CSV_ID
+	APPLICATION_JSON_ID
+	APPLICATION_JSON_PATCH_JSON_ID
+	APPLICATION_LD_JSON_ID
+	APPLICATION_VND_API_JSON_ID
+	APPLICATION_XML_ID
+	APPLICATION_ATOM_XML_ID
+	APPLICATION_RSS_XML_ID
+	APPLICATION_OCTET_STREAM_ID
+	APPLICATION_PDF_ID
+	APPLICATION_ZIP_ID
+	APPLICATION_X_WWW_FORM_URLENCODED_ID
+	APPLICATION_X_HTML_XML_ID
+	APPLICATION_VND_MS_EXCEL_ID
+	APPLICATION_VND_MS_POWERPOINT_ID
+	APPLICATION_MS_WORD_ID
+	MULTIPART_FORM_DATA_ID
+	MULTIPART_BYTERANGES_ID
+	IMAGE_PNG_ID
+	IMAGE_JPEG_ID
+	IMAGE_GIF_ID
+	IMAGE_SVG_XML_ID
+	AUDIO_MPEG_ID
+	AUDIO_OGG_ID
+	AUDIO_WAV_ID
+	VIDEO_MP4_ID
+	VIDEO_OGG_ID
+	VIDEO_WEBM_ID
+
+	TEXT_PLAIN                        = "text/plain"
+	TEXT_HTML                         = "text/html"
+	TEXT_XML                          = "text/xml"
+	TEXT_CSS                          = "text/css"
+	TEXT_JAVASCRIPT                   = "text/javascript"
+	TEXT_CSV                          = "text/csv"
+	APPLICATION_JSON                  = "application/json"
+	APPLICATION_JSON_PATCH_JSON       = "application/json-patch+json"
+	APPLICATION_LD_JSON               = "application/ld+json"
+	APPLICATION_VND_API_JSON          = "application/vnd.api+json"
+	APPLICATION_XML                   = "application/xml"
+	APPLICATION_ATOM_XML              = "application/atom+xml"
+	APPLICATION_RSS_XML               = "application/rss+xml"
+	APPLICATION_OCTET_STREAM          = "application/octet-stream"
+	APPLICATION_PDF                   = "application/pdf"
+	APPLICATION_ZIP                   = "application/zip"
+	APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded"
+	APPLICATION_X_HTML_XML            = "application/xhtml+xml"
+	APPLICATION_VND_MS_EXCEL          = "application/vnd.ms-excel"
+	APPLICATION_VND_MS_POWERPOINT     = "application/vnd.ms-powerpoint"
+	APPLICATION_MS_WORD               = "application/msword"
+	MULTIPART_FORM_DATA               = "multipart/form-data"
+	MULTIPART_BYTERANGES              = "multipart/byteranges"
+	IMAGE_PNG                         = "image/png"
+	IMAGE_JPEG                        = "image/jpeg"
+	IMAGE_GIF                         = "image/gif"
+	IMAGE_SVG_XML                     = "image/svg+xml"
+	AUDIO_MPEG                        = "audio/mpeg"
+	AUDIO_OGG                         = "audio/ogg"
+	AUDIO_WAV                         = "audio/wav"
+	VIDEO_MP4                         = "video/mp4"
+	VIDEO_OGG                         = "video/ogg"
+	VIDEO_WEBM                        = "video/webm"
+)
+
+var ContentTypeList = []string{
+	TEXT_PLAIN,
+	TEXT_HTML,
+	TEXT_XML,
+	TEXT_CSS,
+	TEXT_JAVASCRIPT,
+	TEXT_CSV,
+	APPLICATION_JSON,
+	APPLICATION_JSON_PATCH_JSON,
+	APPLICATION_LD_JSON,
+	APPLICATION_VND_API_JSON,
+	APPLICATION_XML,
+	APPLICATION_ATOM_XML,
+	APPLICATION_RSS_XML,
+	APPLICATION_OCTET_STREAM,
+	APPLICATION_PDF,
+	APPLICATION_ZIP,
+	APPLICATION_X_WWW_FORM_URLENCODED,
+	APPLICATION_X_HTML_XML,
+	APPLICATION_VND_MS_EXCEL,
+	APPLICATION_VND_MS_POWERPOINT,
+	APPLICATION_MS_WORD,
+	MULTIPART_FORM_DATA,
+	MULTIPART_BYTERANGES,
+	IMAGE_PNG,
+	IMAGE_JPEG,
+	IMAGE_GIF,
+	IMAGE_SVG_XML,
+	AUDIO_MPEG,
+	AUDIO_OGG,
+	AUDIO_WAV,
+	VIDEO_MP4,
+	VIDEO_OGG,
+	VIDEO_WEBM,
+}
