324b339ed5fdb746703aaaa29287967855483dbb
Daaaai0809 <dai.tsuruga@80and.co>
2024-08-31 03:01:18 +0900

    feat #52 : implement model generate command

---
+++ cmd/model.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"github.com/Daaaai0809/swagen/cmd/run"
+	input_model "github.com/Daaaai0809/swagen/input/models"
+	"github.com/spf13/cobra"
+)
+
+const LONG_SUMMARY_MODEL = `Generate a model in the swagger yaml file.
+Example:
+  swagen model "fileName" -d "models/example"
+`
+
+var (
+	modeDirName string
+)
+
+var modelCommand = &cobra.Command{
+	Use:   "model [fileName]",
+	Short: "Generate a model schema file in the swagger yaml file",
+	Long:  LONG_SUMMARY_MODEL,
+	Run: func(cmd *cobra.Command, args []string) {
+		fileName := args[0]
+
+		inputs := input_model.NewModelInputs(cmd)
+		inputs.ReadAll()
+
+		params := run.ModelCommandParams{
+			Title:      inputs.GetTitle(),
+			FileName:   fileName,
+			Type:       inputs.GetType(),
+			Properties: inputs.GetProperties(),
+		}
+
+		run.ModelCommandHandler(params, modeDirName)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(modelCommand)
+
+	modelCommand.Flags().StringVarP(&modeDirName, "dir", "d", "", "Directory to save the model schema file")
+
+	modelCommand.MarkFlagRequired("dir")
+}
